a8f13f26ddf79fee3120b7965ef433b2327118c1
Callem Pittard <callem.pittard@gmail.com>
2016-03-05 21:48:35 +0000

    chore(all): updated dependencies to match nav skeletons, inc. karma/isparta

--- build/paths.js
+++ build/paths.js
@@ -1,13 +1,12 @@
-var path = require('path');
-
 var appRoot = 'src/';
+var outputRoot = 'dist/';
 
 module.exports = {
   root: appRoot,
   source: appRoot + '**/*.js',
   html: appRoot + '**/*.html',
   style: 'styles/**/*.css',
-  output: 'dist/',
+  output: outputRoot,
   doc:'./doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/'

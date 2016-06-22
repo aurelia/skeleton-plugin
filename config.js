914f7523c31c1eca213d3183b8165e49287f18da
Rob Eisenberg <rob@bluespire.com>
2016-06-22 12:12:00 -0400

    chore(all): update jspm config

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 System.config({
   defaultJSExtensions: true,
-  transpiler: "none",
+  transpiler: false,
   paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
 
   map: {
-    "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.1.0",
-    "npm:aurelia-polyfills@1.0.0-beta.1.1.0": {
-      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.0"
+    "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-rc.1.0.0",
+    "npm:aurelia-polyfills@1.0.0-rc.1.0.0": {
+      "aurelia-pal": "npm:aurelia-pal@1.0.0-rc.1.0.0"
     }
   }
 });

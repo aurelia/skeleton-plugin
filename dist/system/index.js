9a60e09436e79ed348d58d234f15559ff3f03b1b
Rob Eisenberg <rob@bluespire.com>
2016-07-27 14:21:17 -0400

    chore(all): update build

--- dist/system/index.js
+++ dist/system/index.js
@@ -1,14 +1,16 @@
 'use strict';
 
 System.register([], function (_export, _context) {
+  "use strict";
+
+  function configure(config) {
+    config.globalResources('./hello-world');
+  }
+
+  _export('configure', configure);
+
   return {
     setters: [],
-    execute: function () {
-      function configure(config) {
-        config.globalResources('./hello-world');
-      }
-
-      _export('configure', configure);
-    }
+    execute: function () {}
   };
 });
